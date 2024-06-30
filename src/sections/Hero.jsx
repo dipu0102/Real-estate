5442f6f559e2e8a9d3db0abcbe68b792490d45cc
dipu0102 <50037920+dipu0102@users.noreply.github.com>
2024-06-30 15:52:59 +0530

    hero section added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,110 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import heroimg from "../assets/images/hero1.webp";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useDarkMode } from "../components/DarkModeContext";
 const Hero = () => {
-  return <div>Hero</div>;
+  useEffect(() => {
+    Aos.init({
+      duration: 2000,
+      offset: 200,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  }, []);
+
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
+  return (
+    <>
+      <div className={`${darkMode ? "dark bg-black" : "light bg-white"}`}>
+        <section
+          id="hero"
+          className="w-[95%] h-[600px] m-auto bg-cover bg-center rounded-xl flex justify-center flex-col items-start lg:px-28 px-10 gap-7 z-20"
+          style={{ backgroundImage: `url( ${heroimg})` }}
+        >
+          <h1
+            data-aos="zoom-in"
+            className="text-6xl text-white font-semibold lg:pr-[600px] pr-0 lg:leading-[70px]
+          leading-[60px]"
+          >
+            Find your next Home in Las Vegas
+          </h1>
+          <p
+            data-aos="zoom-in"
+            className="text-white text-xl lg:pr-[500px] pr-0"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+            tempora animi pariatur debitis odio aut sunt sequi non quis magnam!
+          </p>
+        </section>
+      </div>
+      {/* from starts from here  */}
+      <div
+        className={`${
+          darkMode ? "dark bg-black" : "light bg-transparent"
+        } z-10`}
+      >
+        <div
+          data-aos="zoom-in"
+          id="form"
+          className={`${
+            darkMode ? "dark bg-gray-800" : "light bg-white"
+          } lg:w-[70%] w-full m-auto grid lg:grid-cols-4 grid-cols-1 justify-center items-center gap-6 p-8 rounded-xl -mt-14`}
+        >
+          <div className="w-full">
+            <h1 className="text-black font-semibold dark:text-white ">
+              LOCATION
+            </h1>
+            <input
+              type="text"
+              placeholder="Enter an address, state, city or pincode"
+              className="bg-white p-2 w-full mt-2 border-b-[1px] border-[#c9c7c1] outline-none"
+            />
+          </div>
+          <div className="w-full">
+            <h1 className="text-black font-semibold dark:text-white ">TYPE</h1>
+            <select
+              name="selectOption"
+              id="selectOption"
+              className="bg-white p-2 border-b-[1px] w-full border-[#c9c7c1] text-gray-500 text-md"
+            >
+              <option value="" disabled selected>
+                Select Property
+              </option>
+              <option value="Option1">Rentals</option>
+              <option value="Option2">Sales</option>
+              <option value="Option3">Commercial</option>
+            </select>
+          </div>
+
+          <div className="w-full">
+            <h1 className="text-black font-semibold dark:text-white ">
+              CATEGORY
+            </h1>
+            <select
+              name="selectOption"
+              id="selectOption"
+              className="bg-white p-2 border-b-[1px] w-full border-[#c9c7c1] text-gray-500 text-md"
+            >
+              <option value="" disabled selected>
+                Property Category
+              </option>
+              <option value="Option1">Apartments</option>
+              <option value="Option2">Duples</option>
+              <option value="Option3">Condos</option>
+              <option value="Option3">Condos</option>
+            </select>
+          </div>
+          <div className="w-full">
+            <button className="bg-red-600  dark:bg-red-700 hover:bg-black dark:hover:bg-white dark:hover:text-black text-lg p-4 w-full text-white font-semibold rounded-xl cursor-pointer transform hover:scale-110 transition-transform duration-300">
+              SUBMIT
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
